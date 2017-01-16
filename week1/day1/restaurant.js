3fdb33685b4395e805c055828e00a252b11096c6
Yusef Habib <habibfernandez@gmail.com>
2017-01-16 17:33:51 +0100

    Solutions to OOP

---
+++ week1/day1/restaurant.js
@@ -0,0 +1,98 @@
+function Client(name, id) {
+	this.name = name;
+	this.id = id;
+}
+
+function Ingredient(name, cost) {
+	this.name = name;
+	this.cost = cost;
+}
+
+function Dish(name, prize, ingredients) {
+	this.name = name;
+	this.baseCost = 10;
+	this.prize = prize;
+	this.ingredients = ingredients;
+}
+
+Dish.prototype.cost = function() {
+	return this.ingredients.reduce((acc, b) => acc + b.cost, this.baseCost);
+};
+
+Dish.prototype.profit = function() {		
+	return this.prize - this.cost();
+};
+
+
+
+function Restaurant() {
+	this.orders =  {};
+}
+
+Restaurant.prototype.orderDish = function(dish, client) {	
+	if(!!this.orders[client.name])
+		this.orders[client.name].push(dish);
+	else
+		this.orders[client.name] = [dish];
+};	
+
+Restaurant.prototype.printOrders = function() {
+	const clients = Object.keys(this.orders);
+	clients.forEach(client => {
+		this.orders[client].forEach((order, index) => console.log(`Order #${index}: - ${order.cost}`));
+	});
+};
+
+Restaurant.prototype.printCheck = function(client) {
+	let check = 0;
+	console.log(client.name);
+	this.orders[client.name].forEach((order, index) => {
+		console.log(`Order #${index}: - ${order.prize}`);
+		check += order.prize;
+	});
+	console.log(`Total: ${check}\n`);
+}
+
+Restaurant.prototype.totalProfit = function() {
+	let profit = 0;
+	const clients = Object.keys(this.orders);
+	
+	clients.forEach(client => {
+		this.orders[client].forEach(dish => profit += dish.profit());
+	});
+	return profit;
+};
+
+Restaurant.prototype.profitOfACustomer = function(client) {
+	let profit = 0;
+	this.orders[client.name].forEach(dish => profit += dish.profit());
+	return profit;
+};
+
+var cheese = new Ingredient('Cheese', 5);
+var pepperoni = new Ingredient('Pepperoni', 8);
+var dough = new Ingredient('Dough', 2);
+var lettuce = new Ingredient('Lettuce', 1);
+var tomato = new Ingredient('Tomato', 2);
+
+var salad = new Dish('Salad', 30, [lettuce, cheese, tomato]);
+var pizza = new Dish('Pizza', 35, [cheese, pepperoni, dough]);
+var restaurant = new Restaurant();
+
+var pluto = {
+  name: 'Pluto',
+  id: 1
+};
+var goofy = {
+  name: 'Goofy',
+  id: 2
+};
+
+restaurant.orderDish(pizza, goofy);
+restaurant.printCheck(goofy);
+restaurant.orderDish(pizza, pluto);
+restaurant.orderDish(salad, pluto);
+restaurant.printCheck(pluto);
+console.log('The total profit of the restaurant is: ', restaurant.totalProfit());
+console.log('The profit of  the custom Goofy is: ', restaurant.profitOfACustomer(goofy));
+console.log('The profit of  the custom Goofy is: ', restaurant.profitOfACustomer(pluto));
